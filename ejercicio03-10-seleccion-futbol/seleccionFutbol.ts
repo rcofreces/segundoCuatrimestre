1d20c1e919b7c4a987efa8ed5a507128d8e992ec
Ramiro Cofreces <cofrecesrbles@tdf.edu.ar>
2022-09-27 15:42:19 -0300

    27/09/22

--- ejercicio03-10-seleccion-futbol/seleccionFutbol.ts
+++ ejercicio03-10-seleccion-futbol/seleccionFutbol.ts
@@ -19,17 +19,16 @@ export class SeleccionFutbol{
     return this.pais;
   }
   
-  private setPais(pais: string): string {
+  public setPais(pais: string): void {
     this.pais = pais;
-    return this.pais;
   }
 
-  private convocarFutbolista(futbolista: Futbolista): void {
+  public convocarFutbolista(futbolista: Futbolista): void {
     this.listadoFutbolista.push(futbolista);
     console.log(`El futbolista ${futbolista.getNombre()} ${futbolista.getApellido()} fue convocado`);
   }
 
-  private despedirFutbolista(futbolista: Futbolista): void {
+  public despedirFutbolista(futbolista: Futbolista): void {
     for (let i: number = 0; i < this.listadoFutbolista.length; i++) {
       if ((futbolista.getNombre() === this.listadoFutbolista[i].getNombre()) && (futbolista.getApellido() === this.listadoFutbolista[i].getApellido())) {
         this.listadoFutbolista.splice(i, 1);
@@ -39,12 +38,12 @@ export class SeleccionFutbol{
     return console.log(`No se encontró al futbolista ${futbolista.getNombre()} ${futbolista.getApellido()}`);
   }
 
-  private contratarEntrenador(entrenador: Entrenador): void {
+  public contratarEntrenador(entrenador: Entrenador): void {
     this.listadoEntrenador.push(entrenador);
     console.log(`El entrenador ${entrenador.getNombre()} ${entrenador.getApellido()} fue contratado`);
   }
   
-  private despedirEntrenador(entrenador: Entrenador): void {
+  public despedirEntrenador(entrenador: Entrenador): void {
     for (let i: number = 0; i < this.listadoEntrenador.length; i++) {
       if ((entrenador.getNombre() === this.listadoEntrenador[i].getNombre()) && (entrenador.getApellido() === this.listadoEntrenador[i].getApellido())) {
         this.listadoEntrenador.splice(i, 1);
@@ -54,12 +53,12 @@ export class SeleccionFutbol{
     return console.log(`No se encontró al entrenador ${entrenador.getNombre()} ${entrenador.getApellido()}`);
   }
   
-  private contratarMasajista(masajista: Masajista): void {
+  public contratarMasajista(masajista: Masajista): void {
     this.listadoMasajista.push(masajista);
     console.log(`El masajista ${masajista.getNombre()} ${masajista.getApellido()} fue contratado`);
   }
   
-  private despedirMasajista(masajista: Masajista): void {
+  public despedirMasajista(masajista: Masajista): void {
     for (let i: number = 0; i < this.listadoMasajista.length; i++) {
       if ((masajista.getNombre() === this.listadoMasajista[i].getNombre()) && (masajista.getApellido() === this.listadoMasajista[i].getApellido())) {
         this.listadoMasajista.splice(i, 1);
